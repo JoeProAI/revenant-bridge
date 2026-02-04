e0c133b6b847504a94dd6a5cae612d23b3e528f6
JoeProAI <joe@joepro.ai>
2026-02-04 05:43:20 +0000

    Integrate bold maximalist UI for Revenant Bridge

--- server.js
+++ server.js
@@ -1,10 +1,1 @@
-const express = require('express');
-const app = express();
-
-app.get('/health', (req, res) => res.status(200).send('OK'));
-app.get('/test-flow', (req, res) => res.status(200).send('OK'));
-
-const port = process.env.PORT || 3000;
-app.listen(port, '0.0.0.0', () => {
-  console.log('Listening on port ' + port);
-});
+const express = require('express');\nconst app = express();\n\napp.use(express.static('ui'));\n\napp.get('/health', (req, res) => res.status(200).send('OK'));\napp.get('/test-flow', (req, res) => res.status(200).send('OK'));\n\nconst port = process.env.PORT || 3000;\napp.listen(port, '0.0.0.0', () => {\n  console.log('Listening on port ' + port);\n});
